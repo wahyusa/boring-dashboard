a4df869a4225c0e07d1ac7d07176471ab3dc700c
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2023-03-24 12:52:06 +0000

    feat: Dashboard Skeleton WIP

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1 class="text-3xl font-bold underline">Hello Vite with Tailwind !</h1>
-`;
+export const $ = (selector, scope = document) => {
+  return scope.querySelector(selector);
+};
 
-setupCounter(document.querySelector("#counter"));
+export const $$ = (selector, scope = document) => {
+  return Array.from(scope.querySelectorAll(selector));
+};
+
+export const animate = (node, animation, onEnd = function () {}) => {
+  node.classList.add(animation);
+  node.addEventListener(
+    "animationend",
+    function () {
+      node.classList.remove(animation);
+      onEnd(node, animation);
+    },
+    { once: true }
+  );
+};
+
+$("[data-collapse]").addEventListener("click", () => {
+  $("aside").classList.toggle("w-[4rem!important]");
+  $("aside").classList.toggle("[align-items:center!important]");
+  $("main").classList.toggle("pl-[4rem!important]");
+  $$("aside > a").forEach((a) => {
+    a.classList.toggle("[justify-content:center!important]");
+  });
+  $$("[data-collapse-text]").forEach((span) => {
+    span.classList.toggle("hidden");
+  });
+});
